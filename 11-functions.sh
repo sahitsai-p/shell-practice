777dc9d995623078b4f5828cf72dfb6f396f5821
sahitsai-p <polinenisahitsai@gmail.com>
2026-02-08 22:16:23 +0000

    actions

---
+++ 11-functions.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+    echo "please use root user access"
+    exit 1
+fi
+
+validate(){
+    if [$1 -ne 0 ]; then
+        echo "$2..failure"
+        exit 1
+    else
+        echo "$2..success"
+    fi
+}
+
+dnf install nginx -y
+VALIDATE $? "Installing nginx"
+
+dnf install mysql -y
+VALIDATE $? "installing mysql"
