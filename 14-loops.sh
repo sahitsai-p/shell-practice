fcb7939e68f36e85f79d1b10e4f5762bfb6400c0
sahitsai-p <polinenisahitsai@gmail.com>
2026-02-09 20:57:05 +0000

    actions

---
+++ 14-loops.sh
@@ -0,0 +1,26 @@
+#!bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/logs/shell-script"
+LOGS_FILE="/var/logs/shell-script/$0.log"
+if [ $USERID -ne 0 ]; then
+    echo "please use root user access"
+    exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo "$2..failure"
+        exit 1
+    else
+        echo "$2..success"
+    fi
+}
+
+for package in $@
+do
+    dnf install $package -y &>>$LOGS_FILE 
+    VALIDATE $? "$package installation"
+done
